8f343133674c81453e7a32dd406344ef5715e4bd
romankorets <RKorets.98@gmail.com>
2020-01-09 13:20:19 +0000

    delete Result model and migration, refactor Game migration

--- database/migrations/2020_01_08_115841_create_games_table.php
+++ database/migrations/2020_01_08_115841_create_games_table.php
@@ -16,8 +16,9 @@ class CreateGamesTable extends Migration
     {
         Schema::create('games', function (Blueprint $table) {
             $table->bigIncrements('id');
-//            $table->bigInteger("first_player_id");
-//            $table->bigInteger("second_player_id")->nullable();
+            $table->bigInteger("first_player_id");
+            $table->bigInteger("second_player_id")->nullable();
+            $table->bigInteger("winner_id")->nullable();
             $table->string('first_player_color');
             $table->string('second_player_color')->nullable();
             $table->boolean('started')->default(false);
