793b0c7cab018ee56e875823a0364213b8ac28fc
romankorets <RKorets.98@gmail.com>
2020-01-09 13:38:07 +0000

    add fillable fields in User model

--- app/User.php
+++ app/User.php
@@ -17,7 +17,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'rating', 'won_games', 'lost_games', 'draw_games'
     ];
 
     /**
