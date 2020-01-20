1e5c4f8b067840ff41e0817edf1b66e0867db718
romankorets <RKorets.98@gmail.com>
2020-01-20 11:31:48 +0000

    rename BoardComponent to GameComponent

--- resources/views/game/show.blade.php
+++ resources/views/game/show.blade.php
@@ -3,12 +3,12 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <board-component game-Id-Php="{{ $game->id }}" moves-Php="{{ json_encode($game->moves) }}"
+            <game-component game-Id-Php="{{ $game->id }}" moves-Php="{{ json_encode($game->moves) }}"
                                                                  current-User-Id-Php="{{\Illuminate\Support\Facades\Auth::user()->id}}"
                                                                  first-Player-Id-Php="{{$game->first_player_id}}"
                                                                  first-Player-Color-Php="{{$game->first_player_color}}">
 
-            </board-component>
+            </game-component>
         </div>
     </div>
 @endsection
